cd32341ac146c21969c83d9d6d180e0288fa1ebf
Michael Meoak <mmeoak240@gmail.com>
2022-06-30 17:12:31 -0700

    commit tree 594aaf6da4a7c946824a3f5c713b7219a28bba20

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, "Mr. Chocolate", 1, "M", "brown", "happy", 1);
+INSERT INTO bears VALUES (2, "Rowdy", 3, "M", "black", "excited", 1);
+INSERT INTO bears VALUES (3, "Tabitha", 6, "F", "orange", "content", 0);
+INSERT INTO bears VALUES (4, "Sergeant Brown", 10, "M", "blue", "serious", 1);
+INSERT INTO bears VALUES (5, "Melissa", 13, "F", "brown", "distracted", 1);
+INSERT INTO bears VALUES (6, "Grinch", 13, "M", "blue", "grumpy", 1);
+INSERT INTO bears VALUES (7, "Wendy", 17, "M", "green", "happy", 1);
+INSERT INTO bears VALUES (8, null, 21, "M", "light brown", "confused", 0);
